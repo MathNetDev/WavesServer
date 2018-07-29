22ed7923f563f743f063b0ba664285288f991c40
jeroenpost86 <jeroenpost86@gmail.com>
2018-07-29 07:42:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 "use strict";
-//server.js
-var port = 9000;
+
+// Grab arguments given
+var args = process.argv.slice(2);
+
+var port = 8885;
+
+// If we have an argument, use that as the port instead
+// 8889 for live
+// 8888 for test
+// 8887 for dev
+// 8885 for waves
+
+if(args[0]){
+    port = args[0];
+}
+
 var server_sockets = require('./server_sockets');
 var http = require('http');
 
